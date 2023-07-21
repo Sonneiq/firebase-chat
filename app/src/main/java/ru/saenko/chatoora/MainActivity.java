bb748b2f79af9590a3d160cbba8ac4816db9504e
Dmitriy <saenko.3344@gmail.com>
2023-07-21 09:41:02 +0300

    Firebase added

--- app/src/main/java/ru/saenko/chatoora/MainActivity.java
+++ app/src/main/java/ru/saenko/chatoora/MainActivity.java
@@ -4,7 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference myRef = database.getReference("message");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
