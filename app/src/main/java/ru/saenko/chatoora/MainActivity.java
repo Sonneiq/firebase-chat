cee6e2730faf49c1eac628f4bee6b51a12e03093
Dmitriy <saenko.3344@gmail.com>
2023-07-21 09:30:07 +0300

    initial commit

---
+++ app/src/main/java/ru/saenko/chatoora/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.saenko.chatoora;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
